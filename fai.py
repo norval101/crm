cdc4270a4b72f05c55069972b11626b2b0ef2009
Norval Mendez <67773365+norval101@users.noreply.github.com>
2023-03-09 12:39:46 -0500

    Update fai.py

--- fai.py
+++ fai.py
@@ -2,7 +2,7 @@ import openai
 import pyttsx3
 
 # Replace with your OpenAI API key
-openai.api_key = "YOUR_API_KEY"
+openai.api_key = "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
 
 # Initialize the text-to-speech engine
 engine = pyttsx3.init()
